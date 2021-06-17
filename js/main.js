5b70ed0617f81e5e53bfac26983e099919c42760
GelmanB <66279000+GelmanB@users.noreply.github.com>
2021-06-17 20:38:02 +0300

    правки по eslint

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 function getRandomFloat(min, max) {
   if (max > min & min >= 0) {
     return Math.random() * (max - min) + min;
-} else {
+  } else {
+    // eslint-disable-next-line no-alert
     alert('min не может быть меньше нуля или больше max');
-};
-};
+  }
+}
 
 getRandomFloat(0, 100);
